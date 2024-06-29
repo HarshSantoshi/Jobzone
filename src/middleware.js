1909a6c4fa878d2b54ece76c0f4464c13fab8ec8
Harsh <harshsantoshi05@gmail.com>
2024-06-29 17:49:21 +0530

    membership added

---
+++ src/middleware.js
@@ -0,0 +1,13 @@
+// import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
